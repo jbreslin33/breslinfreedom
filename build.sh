4c3b29f59bdebb80332f73c7499ba183e0b905cd
Jim Breslin <jbreslin33@gmail.com>
2018-04-13 20:09:38 -0400

    build

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 echo ---------------------------- build ---------------------
-echo to build
-echo cd ~/sandbox/minetest
-echo cmake . -DRUN_IN_PLACE=TRUE
-echo make 
-
+cmake . -DRUN_IN_PLACE=TRUE
+make 
